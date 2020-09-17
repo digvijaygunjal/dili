2f9d0c3363437cbc2a75c8503b15a35bfeac5f63
Digvijay Gunjal <digvijag@thoughtworks.com>
2020-09-17 10:35:50 +0530

    Add a dataset script for combined dataste

--- src/dili_dataset.py
+++ src/dili_dataset.py
@@ -19,3 +19,23 @@ if __name__ == "__main__":
 
     ncrt_train.to_csv('./data/raw/ncrt_train.csv')
     ncrt_test.to_csv('./data/raw/ncrt_test.csv')
+
+    # Combined
+    xls = pd.ExcelFile('./data/raw/dili_dataset.xlsx')
+    combined_train_data = pd.read_excel(xls, 'supplementary table S2.1', header=1)
+    combined_test_data = pd.read_excel(xls, 'supplementary table S2.2', header=1)
+
+    combined_train = pd.DataFrame()
+    combined_train['cid'] = combined_train_data['CID']
+    combined_train['smiles'] = combined_train_data['Canonical SMILES']
+    combined_train['description'] = combined_train_data['Annotation']
+    combined_train['label'] = combined_train_data['Label']
+
+    combined_test = pd.DataFrame()
+    combined_test['cid'] = combined_test_data['CID']
+    combined_test['smiles'] = combined_test_data['Canonical SMILES']
+    combined_test['description'] = combined_test_data['Annotation']
+    combined_test['label'] = combined_test_data['Label']
+
+    combined_train.to_csv('./data/raw/combined_train.csv')
+    combined_test.to_csv('./data/raw/combined_test.csv')
